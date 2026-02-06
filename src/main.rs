a009967103cf9dadc9db95d050fe41eb9e466027
Vaclav Kucera <vaclav.kucera@signageos.io>
2026-02-06 17:16:28 +0100

    Initial commit - Ratatui example

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+#![forbid(unsafe_code)]
+use crossterm::event;
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+fn main() -> Result<()> {
+    let mut app = App::new();
+    ratatui::run(|terminal| app.run(terminal))
+}
+
+struct App {
+    should_quit: bool,
+    name: String,
+}
+
+impl App {
+    fn new() -> Self {
+        Self {
+            should_quit: false,
+            name: "world".to_string(),
+        }
+    }
+
+    fn run(&mut self, terminal: &mut ratatui::DefaultTerminal) -> Result<()> {
+        while !self.should_quit {
+            terminal.draw(|frame| frame.render_widget("Hello, world!", frame.area()))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
+
+    fn render(&mut self, frame: &mut ratatui::Frame) -> Result<()> {
+        let greeting = format!("Hello, {}!", self.name);
+        frame.render_widget(greeting, frame.area());
+        Ok(())
+    }
+
+    fn handle_events(&mut self) -> Result<()> {
+        if event::read()?.is_key_press() {
+            self.should_quit = true;
+        }
+        Ok(())
+    }
+}
